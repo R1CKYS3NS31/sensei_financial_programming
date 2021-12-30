1ca0ab9cfe7c975e45f5bd337a67be0e31dea644
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2021-12-30 13:18:44 +0300

    subplot amazon commit

--- subplots.py
+++ subplots.py
@@ -10,22 +10,34 @@ end = dt.datetime(2019, 1, 1)
 
 # apple=web.DataReader('AAPL','yahoo',start,end)
 # facebook = web.DataReader('FB','yahoo',start,end)
+# amazon = web.DataReader('AMZN','yahoo',start,end)
 
 # save to a file
 # apple.to_json('apple2019.json')
 # facebook.to_json('facebook2019.json')
+# amazon.to_json('amazon2019.json')
 
 
 # load from a file
 apple = pd.read_json('./apple2019.json')
 facebook = pd.read_json('./facebook2019.json')
+amazon = pd.read_json('./amazon2019.json')
 
-apple['Adj Close'].plot(label= 'AAPL')
-facebook['Adj Close'].plot(label='FB')
+# apple['Adj Close'].plot(label= 'AAPL')
+# facebook['Adj Close'].plot(label='FB')
 # Notice that we are specifying a label , which is important for the legend that helps us to
 # distinguish between the two companies.
 
-plt.ylabel('Adjusted CLose')
-plt.title('Share Price')
-plt.legend(loc='upper left')
+plt.subplot(211)
+apple['Adj Close'].plot(color='blue')
+plt.title('AAPL Share Price')
+plt.xlabel('Date')
+plt.ylabel('Adjusted Close')
+
+plt.subplot(212)
+amazon['Adj Close'].plot()
+plt.xlabel('Date')
+plt.ylabel('Adjusted Close')
+plt.title('AMZN Share Price')
+plt.tight_layout()
 plt.show()
